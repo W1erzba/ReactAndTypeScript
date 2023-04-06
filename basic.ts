8661970339278b2dd3a25e4863a4f0da1a35ffc6
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-04-06 17:40:56 +0200

    primitie values in ts

---
+++ basic.ts
@@ -0,0 +1,15 @@
+// Primitives: numbers, string, boolean  || null, undefine
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+age = 12; // ok
+// age = '12' not ok
+
+let userName: string;
+userName = 'Bartek'; // ok
+
+let isSolider: boolean;
+isSolider = true; // ok
