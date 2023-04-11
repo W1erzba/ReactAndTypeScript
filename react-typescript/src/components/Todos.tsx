f88eb09a83f17ba49c2e668e9f3aef1865767c56
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-04-11 17:41:46 +0200

    added a brand new component to outsource our Todos data

--- react-typescript/src/components/Todos.tsx
+++ react-typescript/src/components/Todos.tsx
@@ -1,11 +1,11 @@
+import TodoItem from './TodoItem';
 import Todo from '../models/todo';
 
-// We can use classes as a Type Aliases to define  our data types
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
 	return (
 		<ul>
 			{props.items.map((item) => (
-				<li key={item.id}>{item.text}</li>
+				<TodoItem key={item.id} text={item.text} />
 			))}
 		</ul>
 	);
